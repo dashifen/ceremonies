def2dfe9b8060c87c9a0e2149c4c75c0f219a601
David Dashifen Kees <dashifen@dashifen.com>
2020-04-13 17:08:36 -0400

    instantiating the correct object now

--- ceremonies.php
+++ ceremonies.php
@@ -5,11 +5,14 @@
  * Description: A WordPress custom post type for the ceremonies on https://memoriam.services.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- * Version: 1.0.0
+ * Version: 1.0.2
  *
  * @noinspection PhpStatementHasEmptyBodyInspection
  * @noinspection PhpIncludeInspection
  */
+
+use Dashifen\Ceremonies\Ceremonies;
+use Dashifen\WPHandler\Handlers\HandlerException;
 
 if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
 elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
@@ -19,7 +22,7 @@ require_once $autoloader;
 
 (function() {
     try {
-        $antiBruteSquad = new AntiBruteSquad();
+        $antiBruteSquad = new Ceremonies();
         $antiBruteSquad->initialize();
     } catch (HandlerException $e) {
         wp_die($e->getMessage());
